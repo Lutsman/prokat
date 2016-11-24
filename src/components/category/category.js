3bb1dca7b20ad5e5c57e20e81c6bd0b8b4d512bf
Dimitry Loutsenko <333333y@gmail.com>
2016-11-24 19:12:09 +0530

    блоки на странице категории

--- src/components/category/category.js
+++ src/components/category/category.js
@@ -111,8 +111,44 @@ angular.module('categoryPage', ['ngAnimate'])
             this.init();
         }
     })
-    .component('categoryGoodsBlock', {
-        
+    .component('categoryGoodsInfo', {
+        templateUrl: 'tmpl/category-goods-info-tmpl.html',
+        bindings: {
+            
+        },
+        controller: function () {
+            this.data = {
+                img: 'img/goods_catalog_preview_1.jpg',
+                traderImg: 'img/trader_logo_3.png',
+                name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                price: '400',
+                currency: '&#8381;',
+                description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                addTime: 'Добавлено 2 часа назад'
+            };
+            this.langServiceData = {
+                more: 'Подробнее',
+                bookmarks: 'В закладки',
+                showMap: 'показать на карте',
+                complain: 'пожаловаться',
+                booking: 'Забронировать',
+                or: 'или',
+                showRoom: 'показать номер'
+            };
+            
+        }
+    })
+    .component('banner', {
+        templateUrl: 'tmpl/banner-tmpl.html',
+        bindings: {
+            data: '=?'
+        },
+        controller: function () {
+            this.data = {
+                img: 'img/banner_1.jpg',
+                href: ''
+            }
+        }
     })
     .component('navbar', {
         templateUrl: 'tmpl/navbar-tmpl.html',
