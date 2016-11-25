d468abe6308710b9490fe318990f4f8a27368b90
Dimitry Loutsenko <333333y@gmail.com>
2016-11-25 21:52:43 +0530

    пагинация

--- src/components/category/category.js
+++ src/components/category/category.js
@@ -44,10 +44,232 @@ angular.module('categoryPage', ['ngAnimate'])
             {}
         ];
     })
+    .filter('range', function () {
+        return function (input, from, to) {
+            if (!input) return [];
+            
+            return input.slice(from, to);
+        }
+    })
+    .filter('sort', function () {
+        return function (input, key, fromLowToHight) {
+            if (!input) return [];
+            
+            let result = input.sort(function (a, b) {
+                if (fromLowToHight) {
+                    if (a[key] > b[key]) return 1;
+                    if (a[key] < b[key]) return -1;
+                } else {
+                    if (a[key] < b[key]) return 1;
+                    if (a[key] > b[key]) return -1;
+                }
+            });
+            
+            return result;
+        }
+    })
     .component('category', {
         templateUrl: 'tmpl/category-tmpl.html',
         controller: function () {
+            this.dataGoods = [
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 355,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 4.3
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 400,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 2
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 500,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 5
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 700,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 1
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 25,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 2.2
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 355,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 3.3
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 171,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 4.2
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 314,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 1.5
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 275,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 3
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 366,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 2.7
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 555,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 4.3
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 576,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 4.9
+                },
+                {
+                    img: 'img/goods_catalog_preview_1.jpg',
+                    traderImg: 'img/trader_logo_3.png',
+                    name: 'Горный профессиональный велосипед двухподвес GT FURY',
+                    price: 1000,
+                    currency: '&#8381;',
+                    description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
+                    addTime: 'Добавлено 2 часа назад',
+                    rate: 0.1
+                }
+            ];
+            this.dataSearch = {
+                currGoodsCount: 1000,
+                trackByOutput: {
+                    name: 'rate',
+                    fromLowToHight: false
+                }
+            };
+            this.goodsRange = {
+                start: [0, 4],
+                end: [4, 8],
+                goodsPerPage: 8,
+                currPage: 1,
+                pageCount: 0
+            };
+           /* this.setCurrPage = (index) => {
+                
+            };*/
+        }
+    })
+    .component('pagination', {
+        templateUrl: 'tmpl/pagination-tmpl.html',
+        bindings: {
             
+        },
+        controller: function () {
+            this.data = {
+                currPage: 16,
+                goodsPerPage: 10,
+                goodsCount: 50
+            };
+            
+            this.setCurrPage = (index) => {
+                this.data.currPage = index;
+            };
+            this.getStartPos = () => {
+                return Math.floor(this.data.currPage / this.data.goodsPerPage) * this.data.goodsPerPage;
+            };
+            this.getPageCount = () => {
+                return Math.ceil(this.data.goodsCount / this.data.goodsPerPage);
+            };
+            this.prev = () => {
+                let start = this.startPos >= this.data.goodsCount ? this.data.goodsCount - this.data.goodsCount % this.data.goodsPerPage : this.startPos;
+                start -= this.data.goodsPerPage*2;
+                this.startPos = start >= 0 ? start : 0;
+                this.repeater = this.getRepeaterArr();
+            };
+            this.next = () => {
+                this.repeater = this.getRepeaterArr();
+            };
+            this.getRepeaterArr = () => {
+                let arr = [];
+                let i = this.startPos >= this.data.goodsCount ? this.data.goodsCount - this.data.goodsCount % this.data.goodsPerPage : this.startPos;
+                let length = i + this.data.goodsPerPage > this.data.goodsCount ? this.data.goodsCount : i + this.data.goodsPerPage;
+                
+                for (i; i < length; i++) {
+                    arr[i] = i + 1;
+                }
+                this.startPos = i;
+                
+                //console.log(arr);
+                
+                return arr;
+            };
+            
+            this.pageCount = this.getPageCount();
+            this.startPos = this.getStartPos();
+            this.repeater = this.getRepeaterArr();
         }
     })
     .component('searchAdvance', {
@@ -114,18 +336,20 @@ angular.module('categoryPage', ['ngAnimate'])
     .component('categoryGoodsInfo', {
         templateUrl: 'tmpl/category-goods-info-tmpl.html',
         bindings: {
+            data: '<'
             
         },
         controller: function () {
-            this.data = {
+            /*this.data = {
                 img: 'img/goods_catalog_preview_1.jpg',
                 traderImg: 'img/trader_logo_3.png',
                 name: 'Горный профессиональный велосипед двухподвес GT FURY',
                 price: '400',
                 currency: '&#8381;',
                 description: 'С этим байком, рама которого аналогична верхней модели, вы будете не только быстрее на даунхильных трассах, но и сможете крепко подружиться в байк-парках. Если вы тот, кто не намерен отступать перед большими трамплинами и самым сложным уклоном.',
-                addTime: 'Добавлено 2 часа назад'
-            };
+                addTime: 'Добавлено 2 часа назад',
+                rate: 5
+            };*/
             this.langServiceData = {
                 more: 'Подробнее',
                 bookmarks: 'В закладки',
