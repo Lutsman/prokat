0b592cb696d7baec4b72a8aaa70b17e36b5e8670
Admin-PC\Admin <333333y@gmail.com>
2016-12-28 23:31:34 +0400

    статьи, как-то работают

--- src/components/articles/articles.component.js
+++ src/components/articles/articles.component.js
@@ -1,13 +1,63 @@
+import angular from 'angular';
+
+
 class ArticlesController {
     constructor(articlesData) {
         this.isHiddenAppNav = true;
         this.articlesData = articlesData;
-        this.articles = this.articlesData.articles;
+        this.getMoreArtStep = 3;
+        this.getMoreArtCurr = 6;
+        this.articles = this.articlesData.articles.slice(0, this.getMoreArtCurr);
+
+        this.labelFilters = [];
+        this.isOtherLabelFilter = false;
+
+
+
+
         this.bannerData = {
             title: 'title',
             img: 'img/articles-banner.jpg',
             href: 'hello'
-        }
+        };
+
+        this.getLabelFilters();
+        this.activelabelFilter = this.labelFilters[0] || '';
+    }
+
+    getLabelFilters() {
+        //this.labelFilters = [''];
+
+        angular.forEach(this.articles, (article) => {
+            if (!article.label) {
+                article.label = 'other';
+                this.isOtherLabelFilter = true;
+                return;
+            }
+
+            let label = article.label.toLowerCase();
+
+            if (label === 'other') return;
+
+            if (!~this.labelFilters.indexOf(label)) {
+                this.labelFilters.push(label);
+            }
+        });
+
+        console.log(this.labelFilters);
+        /*if (this.isOtherLabelFilter) {
+            this.labelFilters.push('other');
+        }*/
+    }
+
+    setActiveLabelFilter(label) {
+        this.activelabelFilter = label;
+    }
+
+    getMoreArticles() {
+        this.getMoreArtCurr += this.getMoreArtStep;
+        this.articles = this.articlesData.articles.slice(0, this.getMoreArtCurr);
+        this.getLabelFilters();
     }
 }
 
